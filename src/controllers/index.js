2d557964089cb09dba3b24e469fe2a372f09ee19
Sam Harris <sam@jam-bc.com>
2019-10-23 22:22:10 +0200

    style and update navbar

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,15 +3,11 @@ const router = express.Router();
 
 const users = require("./users");
 const userPage = require("./userPage");
-// const cravingResults = require("./cravingResults");
-// const product = require("./product");
 const error = require("./error");
 
 router.get("/", (req, res) => res.render("home"));
 router.get("/users", users.get);
 router.get("/user/:id", userPage.get);
-// router.get("/results/:craving", cravingResults.get);
-// router.get("/product/:id", product.get);
 
 router.use(error.client);
 router.use(error.server);
