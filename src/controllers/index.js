43f8c9fe368f5a458b0a38a2f295b9f1fe34ab03
Sam Harris <sam@jam-bc.com>
2019-10-23 22:03:29 +0200

    turn home into welcome page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const home = require("./home");
-// const allResults = require("./allResults");
+const users = require("./users");
+const userPage = require("./userPage");
 // const cravingResults = require("./cravingResults");
 // const product = require("./product");
 const error = require("./error");
 
-router.get("/", home.get);
-// router.get("/results", allResults.get);
+router.get("/", (req, res) => res.render("home"));
+router.get("/users", users.get);
+router.get("/user/:id", userPage.get);
 // router.get("/results/:craving", cravingResults.get);
 // router.get("/product/:id", product.get);
 
