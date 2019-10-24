ce5a23fe44c19b68b76dbfd7cd3f0506c8baae14
Sam Harris <sam@jam-bc.com>
2019-10-24 16:50:09 +0200

    get current time and pause playback at given time

--- public/javascript/dom.js
+++ public/javascript/dom.js
@@ -1,11 +1,11 @@
-console.log("yo yo yo");
-
 var x = document.getElementById("audio-1");
-console.log(x);
 
 function pauseAudio() {
   x.pause();
 }
 
-var y = document.getElementById("card-1");
-console.log(y);
+setInterval(() => {
+  if (x.currentTime >= 30) {
+    pauseAudio();
+  }
+}, 100);
