665167f67d801748bdfc3396345be0bdc1851022
Sam Harris <sam@jam-bc.com>
2019-10-24 11:01:38 +0200

    dom.js manipulating files in the dom

---
+++ public/javascript/dom.js
@@ -0,0 +1,11 @@
+console.log("yo yo yo");
+
+var x = document.getElementById("audio-1");
+console.log(x);
+
+function pauseAudio() {
+  x.pause();
+}
+
+var y = document.getElementById("card-1");
+console.log(y);
